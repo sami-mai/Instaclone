1d867ebf1d96fc00a7ca22a3b28780f86ae43224
sami-mai <samirah.maison@gmail>
2018-05-10 11:21:43 +0300

    add app urls

--- instaApp/urls.py
+++ instaApp/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$', views.home, name='home'),
-    url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^search/', views.search_results, name='search_results'),
 ]
 
 
