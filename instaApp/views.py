232ca20d8e42f5fa2c203c9b7f6392e4f9bdd5af
sami-mai <samirah.maison@gmail>
2018-05-12 16:45:05 +0300

    add comments view function

--- instaApp/views.py
+++ instaApp/views.py
@@ -1,76 +1,107 @@
 from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Image
-from .forms import UpdateProfile, CreateComment, NewImagePost
+from .models import Profile, Image, Comment
+from .forms import EditProfile, CreateComment, NewImagePost, EditUserForm
 
 
 # Create your views here.
 def home(request):
     images = Image.objects.all()
-    return render(request, 'index.html', {"images": images})
+    current_user = request.user
+    return render(request, 'index.html', {"images": images, "current_user": current_user})
 
 
 '''
- limit only an authenticated User to view and update full profile
+ Limited to an authenticated User
  '''
 
 
 @login_required(login_url='/accounts/login/')
-def user_profile(request):
+def user_profile(request, id):
     current_user = request.user
-    profile = Profile.objects.get(user=current_user)
-    # photos = Profile.objects.filter(photos=current_user)
-    return render(request, "dashboard/profile.html", {"profile": profile})
+    try:
+        profile = Profile.objects.get(user=current_user.id)
+        photos = Image.objects.filter(user=current_user.id)
+
+    except DoesNotExists:
+        raise Http404()
+
+    context = {"current_user": current_user, "photos": photos}
+    return render(request, 'dashboard/profile.html', context)
 
 
 @login_required(login_url='/accounts/login/')
-def profile(request):
+def edit_profile(request, id):
     current_user = request.user
-
+    profile = Profile.objects.get(user=current_user.id)
     if request.method == 'POST':
-        form = UpdateProfile(request.POST, request.FILES)
+        form = EditProfile(request.POST, request.FILES)
         if form.is_valid():
             profile = form.save(commit=False)
             profile.user = current_user
             profile.save()
             return HttpResponseRedirect('/accounts/profile')
     else:
-        form = UpdateProfile()
-    # profile = Profile.objects.get(user=current_user)
-    return render(request, 'dashboard/profile.html', {"form": form})
-
-
-def single(request, image_id):
-
-    # images_id = Image.objects.get(id=image_id)
-    if request.method == 'POST':
-        form = CreateComment(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.image = images_id
-            comment.profile = request.user
-            comment.save()
-            HttpResponseRedirect('single')
-    else:
-        form = CreateComment()
-
-    # image = Image.objects.get(id=image_id)
-    comments = Comment.objects.filter(image=image_id)
-    return render(request, 'dashboard/single.html', {"comments": comments, "form": form})
+        profile_form = EditProfile()
+        user_form = EditUserForm()
+    context = {"current_user": current_user, "profile_form": profile_form, "user_form": user_form}
+    return render(request, 'dashboard/edit-profile.html', context)
 
 
 @login_required(login_url='/accounts/login/')
-def create(request):
+def upload_photo(request):
     current_user = request.user
+    current_profile = current_user.profile
     if request.method == 'POST':
         form = NewImagePost(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            post.profile = current_user
+            post.user = current_user
+            post.profile = current_profile
             post.save()
-            HttpResponseRedirect('/accounts/profile')
+            return redirect(user_profile, current_user.id)
     else:
+        uploads_form = NewImagePost()
+    return render(request, 'dashboard/create_post.html', {"uploads_form": uploads_form})
 
-        form = NewImagePost()
-    return render(request, 'dashboard/create_post.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def single_post(request, image_id):
+    '''
+    View function to display a single post, its comments and likes
+    '''
+    current_user = request.user
+    try:
+        photo = Image.objects.get(id=id)
+        title = f'{current_post.user.username}\'s post'
+        comments = Comment.get_comments(id)
+        if request.method == 'POST':
+            comment_form = CreateComment(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.image = images_id
+                comment.profile = request.user
+                comment.save()
+                HttpResponseRedirect('single_post')
+        else:
+            comment_form = CreateComment()
+
+    except DoesNotExist:
+        raise Http404()
+    context = {"title": title, "photo": photo, "comments": comments, "comment_form": comment_form}
+    return render(request, 'dashboard/single.html', context)
+
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'profile' in request.GET and request.GET['profile']:
+            search_term = request.GET.get('profile')
+            searched_photos = Profile.search_profile(search_term)
+            message = f"{search_term}"
+            context = {"message": message, "searched_photos": searched_photos}
+            return render(request, 'all-instagram/search.html', context)
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'dashboard/search.html', {"message": message})
