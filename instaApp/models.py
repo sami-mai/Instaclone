fb8744779e0024c9daf9cd7922f7b2a48a3447f6
sami-mai <samirah.maison@gmail>
2018-05-12 16:49:25 +0300

    update

--- instaApp/models.py
+++ instaApp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from tinymce.models import HTMLField
+from django.dispatch import receiver
 
 
 '''
@@ -9,68 +11,94 @@ tags model class
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=30)
+    '''
+    Tag nodel class that defines categories of posts and tags on posts
+    '''
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
 
+    def save_tag(self):
+        self.save()
 
-'''
-Profile model class
-'''
+    def delete_tag(self):
+        self.delete()
+
+    @classmethod
+    def get_tags(cls):
+        tags = Tag.objects.all()
+        return tags
 
 
 class Profile(models.Model):
+    '''
+    Profile model class
+    '''
     avatar = models.ImageField(upload_to='avatar/', blank=True)
     bio = HTMLField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     photos = models.ImageField(upload_to='uploads/', blank=True)
 
     def __str__(self):
-        return self.user
+        return self.user.username
+
+    # Create Profile when creating a User
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    # Save Profile when saving a User
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
     '''
     Class methods:
+    update_profile:
     find_profile: allows us to search for a user using their profile name.
     get_profile_by_id: allows us to get profile by id.
     '''
+    @classmethod
+    def update_profile(cls, id, bio):
+        update = Image.objects.filter(id=id).update(bio=bio)
+        return update
+
     @classmethod
     def find_profile(cls, search_term):
         profile = cls.objects.filter(user__username__icontains=search_term)
         return profile
 
     @classmethod
-    def get_profile_by_id(cls, user_id):
-        profile = Profile.objects.get(id=user_id)
-        return profile
-
-'''
-Image model class
-'''
+    def get_profiles(cls):
+        profiles = Profile.objects.all()
+        return profiles
 
 
 class Image(models.Model):
+    '''
+    Image model class
+    '''
     image = models.ImageField(upload_to='photos/', blank=True)
     image_name = models.CharField(max_length=60)
     image_caption = HTMLField()
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     likes = models.CharField(max_length=60)
     Comments = models.CharField(max_length=60)
     tags = models.ManyToManyField(Tag)
     post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.image_name
+        return self.user.username
 
-    '''
-    Save an image to database.
-    '''
+    class Meta:
+        ordering = ['post_date']
+
     def save_image(self):
         self.save()
 
-    '''
-    Delete image from database.
-    '''
     def delete_image(self):
         self.delete()
 
@@ -85,17 +113,35 @@ class Image(models.Model):
     Class method that Allows us to get a image using a its ID.
     '''
     @classmethod
-    def get_image_by_id(cls, id):
+    def get_photo_by_id(cls, id):
         pass
 
+    @classmethod
+    def get_photos(cls):
 
-'''
-Comments model class
-'''
+        photos = Image.objects.all()
+
+        return photos
+
+    @classmethod
+    def get_profile_photos(cls, profile_id):
+        profile_images = Image.objects.filter(profile=profile_id).all()
+        return profile_images
 
 
 class Comment(models.Model):
-    comment = models.TextField()
-    image = models.ForeignKey(Image)
-    profile = models.ForeignKey(User)
+    '''
+    Comments model class
+    '''
+    comment = models.TextField(blank=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @classmethod
+    def get_comments(cls, image_id):
+        comments = Comment.objects.filter(image=image_id)
+        return comments
