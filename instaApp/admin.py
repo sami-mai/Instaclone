8b90f1bd9be03bd856917a541c944b12c3eca5c2
sami-mai <samirah.maison@gmail>
2018-05-12 10:25:48 +0300

    register models

--- instaApp/admin.py
+++ instaApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Image, Profile, Tag, Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Tag',)
+
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Tag)
