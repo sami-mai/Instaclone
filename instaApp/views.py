920e9ca3d783e4fe957068ef9cd8a78863ed176e
sami-mai <samirah.maison@gmail>
2018-05-12 21:50:10 +0300

    update edit profile view function

--- instaApp/views.py
+++ instaApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Image, Comment
 from .forms import EditProfile, CreateComment, NewImagePost, EditUserForm
+from django.db import transaction
 
 
 # Create your views here.
@@ -27,25 +28,29 @@ def user_profile(request, id):
     except DoesNotExists:
         raise Http404()
 
-    context = {"current_user": current_user, "photos": photos}
+    context = {"current_user": current_user, "photos": photos, "profile": profile}
     return render(request, 'dashboard/profile.html', context)
 
 
 @login_required(login_url='/accounts/login/')
+@transaction.atomic
 def edit_profile(request, id):
     current_user = request.user
     profile = Profile.objects.get(user=current_user.id)
     if request.method == 'POST':
-        form = EditProfile(request.POST, request.FILES)
-        if form.is_valid():
-            profile = form.save(commit=False)
+        profile_form = EditProfile(request.POST, request.FILES, instance=current_user.profile)
+        user_form = EditUserForm(request.POST, instance=current_user)
+        if profile_form.is_valid() and user_form.is_valid():
+            profile = profile_form.save(commit=False)
+            user = user_form.save(commit=False)
             profile.user = current_user
             profile.save()
-            return HttpResponseRedirect('/accounts/profile')
+            user.save()
+            return redirect('user_profile', current_user.id)
     else:
         profile_form = EditProfile()
         user_form = EditUserForm()
-    context = {"current_user": current_user, "profile_form": profile_form, "user_form": user_form}
+    context = {"current_user": current_user, "profile_form": profile_form, "user_form": user_form, "profile": profile}
     return render(request, 'dashboard/edit-profile.html', context)
 
 
@@ -54,16 +59,16 @@ def upload_photo(request):
     current_user = request.user
     current_profile = current_user.profile
     if request.method == 'POST':
-        form = NewImagePost(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
+        uploads_form = NewImagePost(request.POST, request.FILES)
+        if uploads_form.is_valid():
+            post = uploads_form.save(commit=False)
             post.user = current_user
             post.profile = current_profile
             post.save()
             return redirect(user_profile, current_user.id)
     else:
         uploads_form = NewImagePost()
-    return render(request, 'dashboard/create_post.html', {"uploads_form": uploads_form})
+    return render(request, 'dashboard/create_post.html', {"uploads_form": uploads_form, "current_user": current_user})
 
 
 @login_required(login_url='/accounts/login/')
@@ -78,8 +83,8 @@ def single_post(request, image_id):
         comments = Comment.get_comments(id)
         if request.method == 'POST':
             comment_form = CreateComment(request.POST)
-            if form.is_valid():
-                comment = form.save(commit=False)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
                 comment.image = images_id
                 comment.profile = request.user
                 comment.save()
