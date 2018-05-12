61daa3881c4b6b3d415a1e0f0fc1675fd5d9a31d
sami-mai <samirah.maison@gmail>
2018-05-12 10:20:47 +0300

    update project urls

--- instaProject/urls.py
+++ instaProject/urls.py
@@ -19,7 +19,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('instaApp.urls')),
-    url(r'', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/login'}),
+    url(r'', include('instaApp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
