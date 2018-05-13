973fef24cb9d1124b8ab33d43ce8ba6c90013383
sami-mai <samirah.maison@gmail>
2018-05-13 20:50:44 +0300

    add url for post comments

--- instaApp/urls.py
+++ instaApp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^accounts/profile/(\d+)', views.user_profile, name="user_profile"),
     url(r'^accounts/edit-profile/(\d+)', views.edit_profile, name='edit_profile'),
     url(r'^accounts/upload-photo/', views.upload_photo, name='upload_photo'),
-    url(r'^accounts/single-post/', views.single_post, name='single_post'),
+    url(r'^accounts/post-comment/(/d+)', views.post_comment, name='post_comment'),
     url(r'^search/', views.search_results, name='search_results'),
 ]
 
